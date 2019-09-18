def7be91f6dee4b57024e047a4f36332298a7e00
SolidAlloy <perepelartem@gmail.com>
2019-09-18 16:17:16 +0300

    JobManager, new environment variables, and completed documentation
    - Some functions related to the "at" command were separated in the JobManager class.
    - documentation was completed.
    - Maximum queue length and the time to wait before deleting a debugger file were implemented as the environment variables.

--- easywp-cron/app/functions.py
+++ easywp-cron/app/functions.py
@@ -1,9 +1,8 @@
 import re
-import subprocess
 from functools import wraps
 
-from app import app, cache
-from flask import jsonify, url_for
+from app import app
+from flask import jsonify
 
 # Regular experessions used for the validation of input.
 domain_regex = re.compile(r'^([a-zA-Z0-9][a-zA-Z0-9-_]*\.)*[a-zA-Z0-9]*[a-zA-Z0-9-_]*[a-zA-Z0-9]+$')
@@ -41,6 +40,16 @@ def catch_custom_exception(func):
 
 
 def check_inputs(values_dict):
+    """Validate strings against regular expressions.
+
+    Arguments:
+        values_dict {dict} -- Dictionary with the
+            type_of_input=input_to_validate pairs
+
+    Returns:
+        dict -- Dictionary with the
+            type_of_input=True_if_input_validated pairs
+    """
     result_dict = {}
     for key in values_dict.keys():
         if key == 'domain':
@@ -56,77 +65,18 @@ def check_inputs(values_dict):
     return result_dict
 
 
-def get_queue():
-    result = subprocess.run('atq', capture_output=True, text=True)  # output current queue
-    if result.returncode != 0:
-        raise SystemError('atq is broken.')
-    return result.stdout
-
-
-@cache.cached(timeout=60, key_prefix='queue_length')
-def get_queue_length():
-    queue = get_queue()
-    if not queue:
-        return 0
-    lines = queue.split('\n')[:-1]  # split output into lines
-    return len(lines)
-
-
-def find_job(domain):
-    queue = get_queue()
-    if not queue:
-        return False
-    lines = queue.split('\n')[:-1]  # split output into lines
-    numbers = [line.split('\t', 1)[0] for line in lines]  # get only the job number for each line
-    for number in numbers:
-        output = subprocess.run(['at', '-c', number],
-                                capture_output=True, text=True).stdout
-        if output.find(domain):  # get content of each job until the domain is found
-            return number
-    return False
-
-
-def add_job(domain, file, anaylyze_url):
-    try:
-        job_in_queue = find_job(domain)
-    except SystemError:
-        return [False, '"atq" doesn\'t work on the server.']
-
-    if job_in_queue:
-        return [False, 'Job is already created.']
-    else:
-        if get_queue_length() > 240:
-            return [False, 'Resource is temporarily busy.']
-        result = subprocess.run(
-            ['at', 'now', '+', '2', 'hours'],
-            text=True,
-            input='curl -L -X POST -H "Content-Type:application/x-www-form-urlencoded; charset=UTF-8" -d "domain=' +
-            domain +
-            '&file=' +
-            file +
-            '" ' +
-            url_for('analyze', _external=True) +
-            ' >/dev/null 2>&1'
-        )
-        if result.returncode == 0:
-            return [True, 'Job successfully created.']
-        else:
-            return [False, 'Job creation failed.']
-
-
-def delete_job(job_id):
-    result = subprocess.run(['atrm', str(job_id)])
-    if result.returncode == 0:
-        return [True, 'Job successfully deleted.']
-    else:
-        return [False, 'There is no such job.']
-
-
 def process_failed_inputs(validated_inputs):
-    if not validated_inputs['domain'] and not validated_inputs['file']:  # if the domain wasn't validated against the regex
+    """Check if all inputs were validated and return corresponding values.
+
+    Arguments:
+        validated_inputs {dict} -- Dictionary of the
+            type_of_input->input_string pairs.
+    """
+    # If the domain wasn't validated against the regex
+    if not validated_inputs['domain'] and not validated_inputs['file']:
         success = False
         message = 'Domain and file are invalid.'
-    elif not validated_inputs['domain']:  # if no domain was passed
+    elif not validated_inputs['domain']:  # If the domain wasn't validated
         success = False
         message = 'The domain is invalid.'
     else:
--- easywp-cron/app/models.py
+++ easywp-cron/app/models.py
@@ -4,6 +4,23 @@ from app import db
 
 
 class FailedLink(db.Model):
+    """Database model to store failed debugger removals.
+
+    Extends:
+        db.Model
+
+    Variables:
+        __tablename__ {str} -- Overrides the default name of the table
+            with the custom one.
+        id {object} -- ID of the failed link.
+        link {object} -- URL of the debugger file, including the
+            website it is located at.
+        error {object} -- Short description of the error occurred when
+            the app tried deleting the file.
+        message {object} -- Long description of the error.
+        timestamp {object} -- Timestamp when the failed link was added
+            to the database.
+    """
     __tablename__ = 'failed_links'
     id = db.Column(db.Integer, primary_key=True)
     link = db.Column(db.String(128), index=True, unique=True)
@@ -16,6 +33,19 @@ class FailedLink(db.Model):
 
 
 class BotUser(db.Model):
+    """Database model to store information on the users who installed
+        the bot.
+
+    Extends:
+        db.Model
+
+    Variables:
+        __tablename__ {str} -- Overrides the default name of the table
+            with the custom one.
+        id {object} -- ID of the user in the application's database.
+        user_id {object} -- ID of the user provided by Flock.
+        token {object} -- User's token for use in Flock.
+    """
     __tablename__ = 'bot_users'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(34), index=True, unique=True)
