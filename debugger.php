31afe3bb0025d6954bd167966b0a795fa2a2275f
SolidAlloy <perepelartem@gmail.com>
2019-10-06 07:19:28 +0300

    updated versions in files and regenerated debugger.min.php

--- debugger.php
+++ debugger.php
@@ -19,7 +19,7 @@ session_start();
     !!! Constants section !!!
 */
 
-define('VERSION', '2.2.4');
+define('VERSION', '2.2.5');
 
 // Change it to a more secure password.
 define('PASSWORD', 'notsoeasywp');
